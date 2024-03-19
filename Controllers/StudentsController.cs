c7c453fa74bb3e3a5d31bf251eaf9354dd668723
ranbarlevav <ran.barlevav@gmail.com>
2024-03-20 01:01:02 +0200

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace StudentList.Server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        [HttpGet(Name = "GetStudents")]
+        public IEnumerable<Student> GetStudents(string filterByFirstName = "")
+        {
+
+
+            List<Student> res = new List<Student>();
+            for (int i = 1; i < 2000; i++)
+            {
+                res.Add(new Student()
+                {
+                    IdNumber = "034492181",
+                    FirstName = RandomString(5),
+                    LastName = RandomString(5),
+                    BirthDate = DateTime.Now.AddDays(1 * i),
+                    Gender = Genders.Male,
+                    StudentClass = "ז",
+                    StudentStatus = "לומד ממשיך",
+                    CountryOfOrigin = "ישראל",
+                    StudentType = "ממשיך"
+                });
+            }
+
+
+            return res.ToArray();
+        }
+
+        public static string RandomString(int length)
+        {
+            Random random = new Random();
+
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StudentList.Server
+{
+
+    public enum Genders
+    {
+        [JsonPropertyName("זכר")] 
+        Male = 0,
+        [JsonPropertyName("נקבה")]
+        Femail = 1,
+        [JsonPropertyName("ללא")]
+        None = 2,
+    }
+
+    public class Student
+    {
+        [JsonPropertyName("idnumber")]
+        public string IdNumber { get; set; }
+        [JsonPropertyName("firstname")]
+        public string FirstName { get; set; }
+        [JsonPropertyName("lastname")]
+        public string LastName { get; set; }
+        [JsonPropertyName("birthdate")]
+        public DateTime BirthDate { get; set; }
+        [JsonPropertyName("countryoforigin")]
+        public string CountryOfOrigin { get; set; }
+        [JsonPropertyName("gender")]
+        public Genders Gender { get; set; }
+        [JsonPropertyName("studentclass")]
+        public string StudentClass { get; set; }
+        [JsonPropertyName("studentstatus")]
+        public string StudentStatus { get; set; }
+        [JsonPropertyName("studenttype")]
+        public string StudentType { get; set; }
+    }
+}
